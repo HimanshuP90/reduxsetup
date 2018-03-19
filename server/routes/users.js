cdcc451b0d30c778e78f0233365ff392c3835a3b
HimanshuP90 <himanshup895@gmail.com>
2018-03-19 12:07:31 +0530

    added client side validations

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,30 +1,8 @@
 import express from 'express';
-import Validator from 'validator';
-import isEmpty from 'lodash/isEmpty';
+import validateInput from '../shared/validations/signup';
 
 let router = express.Router();
 
-function validateInput(data) {
-	let errors = {};
-
-  if (Validator.isNull(data.username)) {
-    errors.username = 'This field is required';
-  }
-  if (Validator.isNull(data.password)) {
-    errors.password = 'Password is required';
-  }
-  if (Validator.isNull(data.passwordConfirmation)) {
-    errors.passwordConfirmation = 'Password confirmation is required';
-  }
-  if (!Validator.equals(data.password, data.passwordConfirmation)) {
-    errors.passwordConfirmation = 'Passwords must match';
-  }
-
-  return {
-    errors,
-    isValid: isEmpty(errors)
-  }
-}
 
 router.post('/', (req, res) => {
   setTimeout(() => {
